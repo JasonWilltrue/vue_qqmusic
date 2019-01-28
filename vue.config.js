9386a7685f2187db58c734614a185a68c6aa02a9
lovewcc <148373644@qq.com>
2019-01-28 17:27:00 +0800

    初始化配置文件

---
+++ vue.config.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack')
+const path = require('path')
+
+
+function resolve(dir) {
+  return path.join(__dirname, "..",dir)
+}
+
+module.exports = {
+  css: {
+    loaderOptions: {
+      stylus: {
+        'resolve url': true,
+        'import': [
+          './src/common/less/index.less'
+        ]
+      }
+    }
+  },
+  devServer: {
+     open:true
+  },
+  chainWebpack(config) {
+    config.resolve.alias
+      .set('components', path.resolve(__dirname, 'src/components'))
+      .set('common', path.resolve(__dirname, 'src/common'))
+      .set('api', path.resolve(__dirname, 'src/api'))
+
+    config.plugin('context')
+      .use(webpack.ContextReplacementPlugin,
+        [/moment[/\\]locale$/, /zh-cn/])
+  },
+  baseUrl: ''
+}
