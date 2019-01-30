022e44d1d7d51c0ded6d5622e0610d7ecd597dfd
lovewcc <148373644@qq.com>
2019-01-30 16:06:00 +0800

    歌手页面数据请求

--- src/components/m-singer/singer.vue
+++ src/components/m-singer/singer.vue
@@ -1,15 +1,48 @@
 <template>
-  <div>
-   歌手组件
+  <div
+    class = "singer"
+    ref   = "singerRef"
+  >
+    <router-view></router-view>
+
   </div>
 </template>
 
 <script>
-  export default {
-    name:"singer"
+import MScroll from "base/scroll/scroll";
+import MLoadding from "base/loadding/loadding";
+import { getSingerList } from "api/singer";
+import { ERROR_OK } from "api/config";
+export default {
+  name: "singer",
+  data() {
+    return {
+      singerList: []
+    };
+  },
+  created() {
+    this._getSingerList();
+  },
+  methods: {
+    _getSingerList() {
+      getSingerList().then(res => {
+        if (res.code == ERROR_OK) {
+          console.log(res.data);
+          this.singerList = res.data.list;
+        }
+      });
+    }
   }
+};
 </script>
 
-<style scoped>
-
+<style lang="less" scoped>
+@import "~@/common/less/const.less";
+@import "~@/common/less/mymixin.less";
+.my-singer {
+  position: fixed;
+  top     : 88px;
+  bottom  : 0;
+  width   : 100%;
+}
 </style>
